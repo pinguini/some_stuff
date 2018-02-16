6eb25026138015b50e7474ccc663d0fbb4e670fa
pinguini <mokanton@gmail.com>
2018-02-16 16:55:02 +0700

    add script for rotation gzipping and remove old logs for alfresco

---
+++ rotation_fix.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mkdir -p /opt/citeck/scripts/
+
+
+cat << EOF > /opt/citeck/scripts/gzip_remove.sh
+cd /opt/alfresco*/
+
+#gzipping old logs
+find ./ -maxdepth 1 -iname "*.log.*" -not -iname "*.gz" -ctime +1 -exec gzip {} \;
+#remove old logs
+find ./ -maxdepth 1 -iname "*.log.*" -not -iname "*.gz" -ctime +14 -exec rm -f {} \;
+
+cd ./tomcat/logs/
+find ./ -maxdepth 1 -not -iname "*.gz" -ctime +1 -exec gzip {} \;
+find ./ -maxdepth 1 -not -iname "*.gz" -ctime +14 -exec rm -f {} \;
+EOF
+
+
+chmod +x /opt/citeck/scripts/gzip_remove.sh
+echo "10 *	* * *	root /opt/citeck/scripts/gzip_remove.sh > /dev/null 2>/dev/null" >> /etc/crontab
+
+
+echo -e "/opt/alfresco*/tomcat/logs/catalina.out {\n   copytruncate\n   daily\n   rotate 3\n   compress\n   missingok\n}\n" > /etc/logrotate.d/tomcat-catalina
+
+
+
+
+
